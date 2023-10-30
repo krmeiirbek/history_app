936107a904e98132ddeadc30459e18d5932e812a
kazybekmeiirbek <newkazybek@gmail.com>
2023-10-30 20:29:54 +0600

    add assets and splash page

---
+++ android/app/src/main/kotlin/com/krmeiirbek/historyApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.krmeiirbek.historyApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/krmeiirbek/historyapp/history_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.krmeiirbek.historyapp.history_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
